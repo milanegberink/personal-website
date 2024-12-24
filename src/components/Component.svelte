84d0a468e87d2931fa492165ff6be28c00388296
milanegberink <accounts@milanegberink.com>
2024-12-24 03:21:27 +0100

    Changes

--- src/components/Component.svelte
+++ src/components/Component.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-    import { animate } from "motion"
-    import { onMount} from "svelte";
-    let test: number = $state(1);
+    import {animate, stagger} from "motion"
+    import {onMount} from "svelte";
 
     onMount(() => {
-
+        animate(
+            "li",
+            { x: [-50, 0], opacity: [0,1] },
+            { delay: stagger(0.1) }
+        )
 
     });
+
+    let test: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 </script>
+
+{#each test as x}
+    <li>{x}</li>
+{/each}
+
