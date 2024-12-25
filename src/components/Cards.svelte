a26c80a9546822cdb8526b3f8ad29239ae55205b
Milan <556237@student.saxion.nl>
2024-12-25 01:16:20 +0100

    Navbar

---
+++ src/components/Cards.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let active = $state();
+
+</script>
+
+<ul class="flex gap-4">
+    <li class="min-h-[32.4rem] bg-gray-200 w-full rounded-2xl transition-all duration-500 ease-in-out cursor-pointer">
+
+    </li>
+    <li class="min-h-[32.4rem] bg-gray-200 w-full rounded-2xl transition-all duration-500 ease-in-out cursor-pointer">
+
+    </li>
+    <li class="min-h-[32.4rem] bg-gray-200 w-full rounded-2xl transition-all duration-500 ease-in-out cursor-pointer">
+
+    </li>
+</ul>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,10 @@
+<script>
+    import NavBar from "./NavBar.svelte";
+    import Cards from "./Cards.svelte";
+</script>
+
+
+<section class="w-full h-3/4 border rounded-[3rem]">
+    <NavBar />
+    <Cards />
+</section>
